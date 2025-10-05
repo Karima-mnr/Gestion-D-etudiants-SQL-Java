d6d1d50142e4d11163201d1529769019ea6d4927
karimamnr <karimamnr130@gmail.com>
2025-10-05 19:41:09 +0200

    Gestion d'etudiants using MySQL and java in netbeans

---
+++ Documents/NetBeansProjects/Tp6IHM/src/main/java/com/mycompany/tp6ihm/Tp6IHM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp6ihm;
+
+/**
+ *
+ * @author user22µ
+ */
+public class Tp6IHM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
